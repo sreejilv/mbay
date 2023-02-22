445cf1d73c15668fa1a325281a71d0de6b0b9fe9
siddeeq.sinu <siddeeq.sinu@gmail.com>
2023-02-22 13:09:15 +0400

    shop header, main-rtl change

--- assets/shop/js/main-rtl.js
+++ assets/shop/js/main-rtl.js
@@ -1051,23 +1051,7 @@
         $('.curr-dropdown-active').slideToggle(900);
     });
 
-    /// TYPE HEAD SEARCH
-    // constructs the suggestion engine
-    var states = new Bloodhound({
-        datumTokenizer: Bloodhound.tokenizers.whitespace,
-        queryTokenizer: Bloodhound.tokenizers.whitespace,
-        // `states` is an array of state names defined in "The Basics"
-        local: states
-    });
 
-    $('#bloodhound .typeahead').typeahead({
-        hint: true,
-        highlight: true,
-        minLength: 1
-    }, {
-        name: 'states',
-        source: states
-    });
 
 
 })(jQuery);
