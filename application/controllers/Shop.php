cb04166b089c373931a8c50bc38aa73899828bfe
siddeeq.sinu <siddeeq.sinu@gmail.com>
2023-03-29 19:18:58 +0400

    Update Shop.php

--- application/controllers/Shop.php
+++ application/controllers/Shop.php
@@ -452,20 +452,25 @@ class Shop extends Base_Controller {
         $config["uri_segment"] = 2;
 
 
-        $config['full_tag_open']    = "<ul class='pagination'>";
-        $config['full_tag_close']   = "</ul>";
-        $config['num_tag_open']     = '<li>';
+        $config['next_link']        = 'Next';
+        $config['prev_link']        = 'Prev';
+        $config['first_link']       = false;
+        $config['last_link']        = false;
+        $config['full_tag_open']    = '<ul class="pagination justify-content-center">';
+        $config['full_tag_close']   = '</ul>';
+        $config['attributes']       = ['class' => 'page-link'];
+        $config['first_tag_open']   = '<li class="page-item">';
+        $config['first_tag_close']  = '</li>';
+        $config['prev_tag_open']    = '<li class="page-item">';
+        $config['prev_tag_close']   = '</li>';
+        $config['next_tag_open']    = '<li class="page-item">';
+        $config['next_tag_close']   = '</li>';
+        $config['last_tag_open']    = '<li class="page-item">';
+        $config['last_tag_close']   = '</li>';
+        $config['cur_tag_open']     = '<li class="page-item active"><span class="page-link">';
+        $config['cur_tag_close']    = '<span class="sr-only"></span></span></li>';
+        $config['num_tag_open']     = '<li class="page-item">';
         $config['num_tag_close']    = '</li>';
-        $config['cur_tag_open']     = "<li class='disabled'><li class='active'><a >";
-        $config['cur_tag_close']    = "<span class='sr-only'></span></a></li>";
-        $config['next_tag_open']    = "<li>";
-        $config['next_tagl_close']  = "</li>";
-        $config['prev_tag_open']    = "<li>";
-        $config['prev_tagl_close']  = "</li>";
-        $config['first_tag_open']   = "<li>";
-        $config['first_tagl_close'] = "</li>";
-        $config['last_tag_open']    = "<li>";
-        $config['last_tagl_close']  = "</li>";
 
         $this->pagination->initialize($config);
 
